37a4d39ee5697f48be10ed7716ae608243c3d95a
Neil Manimtim <31807992+nman03@users.noreply.github.com>
2017-10-02 19:02:03 -0700

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,11 +1,12 @@
 
 public class BinarySearch {
-
+	
+	// Using loops
 	public static int nonRecursive(int[] arr, int key) {
 		int high = arr.length - 1;
 		int low = 0;
 
-		while (high != low) {
+		while (high >= low) {
 			int mid = (high + low) / 2;
 
 			if (key < arr[mid])
@@ -16,10 +17,33 @@ public class BinarySearch {
 				return mid;
 		}
 
-		return -1;
+		return -1; // when key is not in the list
+	}
+	
+	// Helper Method
+	public static int recursive(int[] arr, int key) {
+		return recursive(arr, 0, arr.length - 1, key);
 	}
 
+	// Using recusions
+	public static int recursive(int[] arr, int low, int high, int key) {
+		
+		int mid = (high + low) / 2;
+			
+		if (high >= low) {
+			if (key < arr[mid]) 
+				return recursive(arr, low, mid - 1, key);
+			else if (key > arr[mid])
+				return recursive(arr, mid + 1, high, key);
+			else 
+				return mid;
+		}
+		
+		return -1; // when key is not in the list
+			
+	}
 
+	// Main
 	public static void printArr(int[] arr) {
 		for (int a : arr)
 			System.out.print(a + " ");
@@ -27,11 +51,11 @@ public class BinarySearch {
 		System.out.println();
 	}
 
-	// Testing
 	public static void main(String[] args) {
 		int[] list = {1, 3, 5, 7, 8, 10, 14};
 		
 		printArr(list);
-		System.out.println("Key is in index: " + nonRecursive(list, 11));
+		System.out.println("(Non-recursive) Key is in index: " + nonRecursive(list, 1));
+		System.out.println("(Recursive) Key is in index: " + recursive(list, 7));
 	}
 }
